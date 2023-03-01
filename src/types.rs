57292bfe90e4d7d3ca47980db0e65efb85922d97
Nick Cao <nickcao@nichi.co>
2023-03-01 12:50:32 +0800

    refactor types

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,4 @@
-use crate::HOMA_MAX_BPAGES;
+use crate::consts;
 use libc::{c_int, c_void, size_t};
 
 #[allow(non_camel_case_types)]
@@ -26,13 +26,15 @@ pub struct homa_recvmsg_args {
     pub flags: c_int,
     pub num_bpages: u32,
     pub pad: [u32; 2],
-    pub bpage_offsets: [u32; HOMA_MAX_BPAGES],
+    pub bpage_offsets: [u32; consts::HOMA_MAX_BPAGES],
 }
 
-#[cfg(test)]
-mod test {
-    #[test]
-    fn home_set_buf_args_size() {
-        // assert_eq!();
-    }
+#[allow(non_camel_case_types)]
+#[repr(C)]
+#[derive(Debug)]
+pub struct homa_abort_args {
+    pub id: u64,
+    pub error: c_int,
+    pub pad1: c_int,
+    pub pad2: [u64; 2],
 }
